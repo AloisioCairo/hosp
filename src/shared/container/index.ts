8330b193ef9a5b5951273349474dc6e2ac4c37c6
unknown <aloisiocairo@gmail.com>
2022-06-06 23:23:15 -0300

    feat: Created the table table container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,15 +1,23 @@
 import { container } from "tsyringe";
+
 import { UsersRepository } from "../../modules/registers/accounts/infra/repositories/UsersRepository";
 import { IUsersRepository } from "../../modules/registers/accounts/repositories/IUsersRepository";
+import { TableRepository } from "../../modules/registers/tables/infra/repositories/TableRepository";
+import { ITableRepository } from "../../modules/registers/tables/repositories/ITableRepository";
 import { HealthInsurancesRepository } from "../../modules/registers/healthInsurances/infra/repositories/HealthInsurancesRepository";
 import { IHealthInsurancesRepository } from "../../modules/registers/healthInsurances/repositories/IHealthInsurancesRepository";
-
-container.registerSingleton<IHealthInsurancesRepository>(
-    "HealthInsurancesRepository",
-    HealthInsurancesRepository
-)
 
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+)
+
+container.registerSingleton<ITableRepository>(
+    "TablesRepository",
+    TableRepository
+)
+
+container.registerSingleton<IHealthInsurancesRepository>(
+    "HealthInsurancesRepository",
+    HealthInsurancesRepository
 )
